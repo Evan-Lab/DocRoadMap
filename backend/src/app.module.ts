ab7921e3855144d4493d4c3dd81134fa06db278a
BaptistePer <baptiste.perard@epitech.eu>
2024-10-24 17:51:55 +0200

    feat(Connect Sever API to DB): Connect the server API to the postgres database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234',
+      database: 'DocRoadMapDatabase',
+      entities: ['dist/**/.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
