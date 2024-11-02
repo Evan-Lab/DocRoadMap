2a0e6f2a94b92de456b4ad3865e87b0f9d8ac729
Evan Labourdette <evan.labourdette@outlook.com>
2024-11-02 13:30:37 -0700

    feat(Add request to backend) no working request, in wait for the backend to open the cors

--- frontend-extension/src/(site)/register/register.tsx
+++ frontend-extension/src/(site)/register/register.tsx
@@ -1,9 +1,12 @@
+import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./register.css";
 
 function Register() {
   const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -14,13 +17,53 @@ function Register() {
       setError("Les mots de passe ne correspondent pas");
       return;
     }
-    navigate("/login");
+
+    console.log("Data to be sent to backend:", {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      processIds: [],
+    });
+
+    axios
+      .post("http://localhost:3000/users/create", {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        processIds: [],
+      })
+      .then((response) => {
+        console.log("User registered successfully");
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.error("Error registering user:", error);
+        setError("Une erreur s'est produite lors de l'inscription");
+      });
   };
 
   return (
     <div className="register-container">
       <h1>Inscription</h1>
       {error && <p style={{ color: "red" }}>{error}</p>}
+      <div className="input-group">
+        <label>Prénom</label>
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label>Nom de famille</label>
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
       <div className="input-group">
         <label>Adresse e-mail</label>
         <input
