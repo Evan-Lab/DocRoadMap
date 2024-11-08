52a1c95816e40bad0190c65a92da8da0783f8103
Evan Labourdette <evan.labourdette@outlook.com>
2024-11-07 21:22:02 -0800

    feat(Update register page)

--- frontend-extension/src/Home.tsx
+++ frontend-extension/src/Home.tsx
@@ -1,12 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className="home-container">
       <h1>Bienvenue sur DocRoadMap</h1>
-      <p>Vous êtes sur la page d'accueil.</p>
       <button onClick={() => navigate("/login")}>Se connecter</button>
       <button onClick={() => navigate("/register")}>S'inscrire</button>
     </div>
