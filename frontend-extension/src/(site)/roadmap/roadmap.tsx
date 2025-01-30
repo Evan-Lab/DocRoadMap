e0209b26b4aa10ce22d85d6612b478b1c5c6a063
Evan Labourdette <evan.labourdette@outlook.com>
2025-01-29 16:57:29 -0800

    feat(add Roadmap page)

---
+++ frontend-extension/src/(site)/roadmap/roadmap.tsx
@@ -0,0 +1,14 @@
+import "./roadmap.css";
+
+function Roadmap() {
+  return (
+    <div className="menu-container">
+      <div className="menu-header">
+        <h1>Bienvenue sur vos Roadmaps</h1>
+      </div>
+      <div className="menu-buttons"></div>
+    </div>
+  );
+}
+
+export default Roadmap;
