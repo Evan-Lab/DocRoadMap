8c4a265005145035da89f28bc9799aa8c2a4aebe
nadia <nadia.moulouad@epitech.eu>
2025-02-19 20:49:02 +0000

    feat roadmap creation with the click of a button and roadmap cards display (hard coded for now)

--- frontend-extension/src/(site)/roadmapView/roadmapView.tsx
+++ frontend-extension/src/(site)/roadmapView/roadmapView.tsx
@@ -1,6 +1,30 @@
 import { FaArrowLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 import "./roadmapView.css";
+
+const token = localStorage.getItem("token");
+console.log(token);
+
+const fetchProcesses = async () => {
+  try {
+    const response = await axios.get("http://localhost:8080/users/me", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(response.data.process);
+    return {
+      data: response.data,
+      error: null,
+    };
+  } catch (error) {
+    return {
+      error: "Unauthorized access. You do not have permission.",
+    };
+  }
+};
 
 const RoadmapView: React.FC = () => {
   const navigate = useNavigate();
@@ -12,13 +36,15 @@ const RoadmapView: React.FC = () => {
     { id: 4, title: "Créer mon entreprise", process: "Creating business plan", progress: 10 },
     { id: 5, title: "Demander ma retraite", process: "Collecting documents", progress: 30 },
   ];
-
+  
+  const cards = fetchProcesses() 
   return (
     <div className="roadmap-container">
       <button className="back-button" onClick={() => navigate(-1)}>
         <FaArrowLeft />
       </button>
       <h1 className="roadmap-title">Mes Roadmaps </h1>
+      <button className="fetch-process-button" onClick={() => console.log(cards)}>click me</button>
       <div className="carousel-container">
         {roadmaps.map((roadmap) => (
           <div className="card">
@@ -41,25 +67,3 @@ const RoadmapView: React.FC = () => {
 };
 
 export default RoadmapView;
-
-
-// import React from "react";
-// import "./Card.css"; 
-// const Card = ({ title, process, progress, onContinue }) => { 
-//   return (
-//     <div className="card">
-//     <div className="card-header"> 
-//       <h3>{title}</h3> </div> 
-//       <div className="card-body"> 
-//         <p className="process">{process}</p> 
-//         <div className="progress-bar"> 
-//           <div className="progress" style={{ width: `${progress}%` }} ></div> 
-//         </div> 
-//         <p>{progress}% completed</p> 
-//         <button className="chat-button">Chat with Assistant</button> 
-//         <button className="continue-button" onClick={onContinue}> Continue </button> 
-//       </div> 
-//     </div> 
-//   );
-// };
-//  export default Card;
