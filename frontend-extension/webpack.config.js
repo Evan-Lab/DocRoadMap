ed4cb80690c9f9c0e97c0f527010bb5bfbc56dff
Evan Labourdette <evan.labourdette@outlook.com>
2025-02-19 22:31:41 -0800

    feat(Add copy in the webpack config)

--- frontend-extension/webpack.config.js
+++ frontend-extension/webpack.config.js
@@ -30,7 +30,10 @@ module.exports = {
   },
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: "manifest.json", to: "../manifest.json" }],
+      patterns: [
+        { from: "manifest.json", to: "../manifest.json" },
+        { from: "content.js", to: "../content.js" },
+      ],
     }),
     ...getHtmlPlugins(["index"]),
   ],
