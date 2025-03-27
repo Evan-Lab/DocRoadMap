99470d008b3c434f1a1ea183e1a3bfbf7a7733d1
Baptiste PERARD <baptiste.perard@epitech.eu>
2025-03-27 22:59:12 +0100

    feat(RAG): Adding RAG with ChromaDB persistent vectorial database, adding method to communicate with the ChromaDB (in a container Docker)

--- ia/rag/rag.py
+++ ia/rag/rag.py
@@ -1,6 +1,9 @@
-from .embedding import EmbeddingFunction
+from embedding import EmbeddingFunction
 import chromadb
 import uuid
+import os
+
+CHROMA_HOST = os.getenv('CHROMA_HOST', 'localhost')
 
 class RAG:
     def __init__(self, collection_name, chunk_size=2048, distance_search=5):
@@ -8,12 +11,12 @@ class RAG:
         self.chunk_size = chunk_size
         self.distance_search = distance_search
 
-        self.chroma_client = chromadb.HttpClient(host='localhost', port=8000)
+        self.chroma_client = chromadb.HttpClient(host=CHROMA_HOST, port=8000)
     
     def split_text(self, text):
         try:
             chunks = [text[i:i + self.chunk_size] for i in range(0, len(text), self.chunk_size)]
-            # print("[LOGS RAG] The text was successfully split")
+            print("[LOGS RAG] The text was successfully split")
             return chunks
         except Exception as e:
             raise
@@ -22,18 +25,18 @@ class RAG:
         try:
             embedding = EmbeddingFunction()
             collection = self.chroma_client.get_or_create_collection(name=self.collection_name)
-            # print(f"[LOGS RAG] Success {self.collection_name} collection recovery")
+            print(f"[LOGS RAG] Success {self.collection_name} collection recovery")
             return collection
         except Exception as e:
-            # print(f"[ERROR RAG] Error while retrieving {self.collection_name} collection: {e}")
+            print(f"[ERROR RAG] Error while retrieving {self.collection_name} collection: {e}")
             raise
     
     def delete_collection(self):
         try:
             self.chroma_client.delete_collection(self.collection_name)
-            # print(f"[LOGS] {self.collection_name} deleted with success")
+            print(f"[LOGS] {self.collection_name} deleted with success")
         except Exception as e:
-            # print(f"[ERROR RAG] Error during the deletion of the collection {self.collection_name}: {e}")
+            print(f"[ERROR RAG] Error during the deletion of the collection {self.collection_name}: {e}")
             raise
 
     def add_knowledge(self, chunks):
@@ -45,9 +48,9 @@ class RAG:
                 ids=[f"ids_{i}" for i in range(len(chunks))]
             )
 
-            # print(f"[LOGS RAG] Documents successfully added to the {self.collection_name} collection")
+            print(f"[LOGS RAG] Documents successfully added to the {self.collection_name} collection")
         except Exception as e:
-            # print(f"[ERROR RAG] Error adding document to {self.collection_name} collection: {e}")
+            print(f"[ERROR RAG] Error adding document to {self.collection_name} collection: {e}")
             raise
 
     def query(self, user_input):
@@ -61,5 +64,5 @@ class RAG:
             )
             return result_query['documents']
         except Exception as e:
-            # print(f"[ERROR RAG] Error during execution of request to {self.collection_name} collection: {e}")
+            print(f"[ERROR RAG] Error during execution of request to {self.collection_name} collection: {e}")
             raise
