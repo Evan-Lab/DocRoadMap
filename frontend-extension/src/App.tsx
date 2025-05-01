e605225fefd55e612d6ef770f9112babc1d37f62
Evan Labourdette <evan.labourdette@outlook.com>
2025-04-30 18:33:47 -0700

    feat(Refactor): - Remove Chatbot and Roadmap components; - Update Profile component
    - Removed RoadmapCreation and RoadmapView components
    - Updated the DocroadmapHome component to navigate to the new Profile component.
    - Cleaned up SettingsMenu to remove unused buttons and styles.
    - Adjusted App routing to reflect the removal of deprecated components.

--- frontend-extension/src/App.tsx
+++ frontend-extension/src/App.tsx
@@ -10,13 +10,8 @@ import Login from "./(site)/Connexion/login/login";
 import Register from "./(site)/Connexion/register/register";
 import RegisterConfirmation from "./(site)/Connexion/registerConfirmation/registerConfirmation";
 
-import Chatbot from "./(site)/DocRoadMap/chatbotDonna/chatbot";
 import DocroadmapHome from "./(site)/DocRoadMap/docroadmapHome/docroadmapHome";
-import RoadmapCreation from "./(site)/DocRoadMap/roadmapCreation/roadmapCreation";
-import RoadmapView from "./(site)/DocRoadMap/roadmapView/roadmapView";
-
-import SettingsMenu from "./(site)/Settings/Menu/settingsMenu";
-import Profile from "./(site)/Settings/profile/profile";
+import Profile from "./(site)/DocRoadMap/profile/profile";
 
 function App() {
   return (
@@ -30,13 +25,8 @@ function App() {
             path="/account-confirmation"
             element={<RegisterConfirmation />}
           />
-          <Route path="/chatbot" element={<Chatbot />} />
-          <Route path="/roadmap-view" element={<RoadmapView />} />
-          <Route path="/create-roadmap" element={<RoadmapCreation />} />
           <Route path="/roadmap" element={<DocroadmapHome />} />
-
           <Route path="/profile" element={<Profile />} />
-          <Route path="/settings" element={<SettingsMenu />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
