40cff562e17964ff24c577957123469b3b093ad6
BaptistePer <baptiste.perard@epitech.eu>
2025-06-13 19:19:32 +0200

    feat(Confirmation Email): Adding confirmation of email

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,6 +10,9 @@ import { AiModule } from './ai/ai.module';
 import { ListAdministrativeProcessModule } from './list_administrative_process/list_administrative_process.module';
 import { AiHistoryModule } from './ai_history/ai_history.module';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -23,9 +26,30 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.POSTGRES_USER || 'postgres',
       password: process.env.POSTGRES_PASSWORD || 'postgres',
       database: process.env.POSTGRES_DB || 'postgres',
-      entities: ['dist/**/.entity{.ts,.js}'],
+      entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
       autoLoadEntities: true,
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: parseInt(process.env.MAIL_PORT) || 465,
+        secure: true,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD,
+        },
+      },
+      defaults: {
+        from: '"DocRoadMap" <no_reply@docroadmap.fr>',
+      },
+      template: {
+        dir: join(__dirname, '..', 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
     }),
     UsersModule,
     ProcessModule,
